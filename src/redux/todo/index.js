ebf6d4feb6f53d3eb98643af5489f20112e2f4f1
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2022-01-07 03:23:35 +0300

    created skeloton redux

---
+++ src/redux/todo/index.js
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+    name:'todo',
+    initialState:[
+
+    ],
+    reducers:{
+        addTodo:(state,action)=>{
+            initialState.push(action.payload);
+
+        }
+
+    }
+})
+
+export const {addTodo}=todoSlice.actions;
+export default todoSlice.reducer;
