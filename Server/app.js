470577b3a75b2d78fd28311a06d459f2752b0035
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2022-01-14 04:39:30 +0300

    test3 ci/cd

--- Server/app.js
+++ Server/app.js
@@ -34,7 +34,7 @@ mongoose.connect(
 
 const PORT = process.env.PORT || 5001
 if(process.env.NODE_ENV !=='test'){
-    app.listen(PORT,host,()=>{
+    app.listen(PORT,()=>{
         console.log(`server runing on http://${host}:${PORT}`);
     });
 }
